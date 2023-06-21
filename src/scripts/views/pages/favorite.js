26cde9d111488fee85a945ae7b372823d06d8887
Salsa Dewinta <82956295+salsadewinta@users.noreply.github.com>
2023-06-21 16:46:09 +0700

    Update favorite.js

--- src/scripts/views/pages/favorite.js
+++ src/scripts/views/pages/favorite.js
@@ -1,17 +1,25 @@
 import FavoriteRestaurant from '../../data/favorite-idb';
 import { createRestoItemTemplate } from '../templates/template-creator';
-
+ 
 const Favorite = {
   async render() {
     return `
     <section class="content">
     <div class="headline-content">
-      <h1 tabindex="0" class="headline-title">Let's Visit to Organic's Kitchen</h1>
-      <div id="restaurant" class="post">
-      </div>
+      <h1 tabindex="0" class="headline-title">Restaurant Favorite Kamu</h1>
+      <div id="restaurant" class="post"></div>
     </div>
     `;
   },
+ 
+  async afterRender() {
+    const restaurants = await FavoriteRestaurant.getAllRestaurants();
+    const restaurantsContainer = document.querySelector('#restaurant');
+ 
+    restaurants.forEach((restaurant) => {
+      restaurantsContainer.innerHTML += createRestoItemTemplate(restaurant);
+    });
+  }
 };
-
+ 
 export default Favorite;
